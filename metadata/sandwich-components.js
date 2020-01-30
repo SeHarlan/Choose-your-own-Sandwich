d8284aaf704a965aa90cb49d590b736b63a2ae22
Scott Harlan <seharlan@gmail.com>
2020-01-29 17:58:05 -0800

    skeliton down

---
+++ metadata/sandwich-components.js
@@ -0,0 +1,56 @@
+export default {
+    'breads': {
+        'white': {
+            id: 'white',
+
+        },
+        'potato': {
+            id: 'potato',
+
+        },
+        'rye': {
+            id: 'rye',
+
+        },
+        'lettuce-wrap': {
+            id: 'lettuce-wrap',
+
+        }
+    },
+    'greens': {
+        'spinach': {
+            id: 'spinach',
+
+        },
+        'lettuce': {
+            id: 'lettuce',
+
+        },
+        'tomato': {
+            id: 'tomato',
+
+        },
+        'double-meat': {
+            id: 'double-meat',
+
+        }
+    },
+    'meats': {
+        'turkey': {
+            id: 'turkey',
+
+        },
+        'ham': {
+            id: 'ham',
+
+        },
+        'bacon': {
+            id: 'bacon',
+
+        },
+        'more-green': {
+            id: 'more-green',
+
+        }
+    },
+};
